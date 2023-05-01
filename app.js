a021ee79de140ad8886020fa5a28e0f3df0b46e2
muhammadriazanus <riazanus568@gmail.com>
2023-05-01 08:23:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log(document.body.childNodes)
+// let a = document.body.childNodes
+// console.log(a.parentNode)
+// console.log(a.parentElement)
+let b = document.body
+console.log(b)
+console.log("first chlid of b is:", b.firstElementChild)
